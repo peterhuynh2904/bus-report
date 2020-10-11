d756d4572d605bee8033afa495ffdfaa5386a630
peterhuynh_2904 <peterhuynh2904@gmail.com>
2020-10-11 11:42:50 +1100

    [feature/copy-matrix] Add copy matrix pipe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { CopyMatrixPipeModule } from './shared/pipes/copy-matrix/copy-matrix.module';
+import { PipesTestingModule } from './shared/pipes/testing/pipes.testing.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [CopyMatrixPipeModule, PipesTestingModule],
       declarations: [AppComponent]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  describe('Component', () => {
+    it('should create the app', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it("should have pageTitle 'app.pageTitle'", () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.pageTitle).toEqual('app.pageTitle');
+    });
   });
 
-  it("should have as title 'Bus report'", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.pageTitle).toEqual('Bus report');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.js-page-title').textContent).toContain('Bus report');
+  describe('View', () => {
+    it('should render title', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const element = fixture.debugElement.nativeElement;
+      expect(element.querySelector('.js-page-title').textContent).toContain('fake-copy-matrix__app.pageTitle');
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  pageTitle: string = 'Bus report';
+  pageTitle: string = 'app.pageTitle';
 }
